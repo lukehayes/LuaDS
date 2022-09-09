beab6bd2e1043075d6b997fb8fe818f6bb38ce10
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-09 16:59:52 +0100

    Updated main.lua with latest test node.

--- main.lua
+++ main.lua
@@ -1,59 +1,18 @@
-local tree = {left = {}, right = {}}
+local list = require("ds/list")
+local node = require("ds/node")
 
-function node(value, next_node)
-    local node = {
-        value = value,
-        next_node = next_node,
-    }
+local l = list:new(1)
+l:pushFront(2)
+l:pushFront(3)
+l:pushFront(4)
+l:pushBack(555)
 
-    node.__tostring = function() return "Node Value " .. node.value end
+--for i = 1,10 do
+    --l:pushFront(i* 100)
+--end
 
-    return node
-end
+l:print()
 
-local list = {head = nil}
-
-function listAddNodeFront(list, node)
-    local temp = list.head
-    list.head = node
-    node.next_node = temp
-end
-
-function listAddNodeEnd(list, node)
-    local head = list.head
-    local current = list.head
-
-    while current.next_node ~= nil do
-        current = current.next_node
-    end
-
-    current.next_node = node
-end
-
-
-local a = node(1,nil)
-local b = node(2,a)
-local c = node(3,b)
-local d = node(4,c)
-local e = node(5,d)
-local f = node(6,e)
-
-list.head = f
-
-listAddNodeFront(list, node(777))
-listAddNodeFront(list, node(99))
-listAddNodeEnd(list, node(1001))
-listAddNodeEnd(list, node(22))
-
-function pList(list)
-    local current = list.head
-    while current.next_node ~= nil do
-        print(current.value)
-        current = current.next_node
-        if(current.next_node == nil) then
-            print(current.value)
-        end
-    end
-end
-
-pList(list)
+--print(l.head.value)
+--print(l.head.next_node.value
+--print(l.head.next_node.next_node.value
